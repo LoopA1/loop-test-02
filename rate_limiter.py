9df31183422444d78ab2d31a2390239b869c6fa0
RomKadria <romkadria@gmail.com>
2026-03-05 16:59:49 +0200

    Apply suggestion from @RomKadria

--- rate_limiter.py
+++ rate_limiter.py
@@ -8,11 +8,4 @@ class RateLimiter:
         pass
 
     def is_rate_limited(self, client_ip: str) -> bool:
-        now = time.time()
-        if client_ip not in _request_log:
-            _request_log[client_ip] = []
-        _request_log[client_ip] = [ts for ts in _request_log[client_ip] if ts > now - WINDOW_SECONDS]
-        if len(_request_log[client_ip]) >= MAX_REQUESTS:
-            return True
-        _request_log[client_ip].append(now)
-        return False
+        return True
